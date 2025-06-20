bca3742a7f78cbed5577729c30f8271d75916fc5
karthik <karthikanaboina@gmail.com>
2025-06-20 17:08:47 +0530

    Fix CORS config

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,10 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://frontend-2qseyc1o1-karthikanaboina-5000s-projects.vercel.app']
+}));
+
 app.use(express.json());
 
 app.use('/api/tasks', taskRoutes);
